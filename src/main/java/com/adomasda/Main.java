9b06f4fe0c5ecdab083516ad45d5fa0d1a64111b
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-14 21:54:47 +0300

    tesing. using hashmaps to map strings to methods to avoid if statments

--- src/main/java/com/adomasda/Main.java
+++ src/main/java/com/adomasda/Main.java
@@ -1,7 +1,59 @@
 package com.adomasda;
 
+import java.util.HashMap;
+
+enum SomeEnum {
+    FIRST_FLOOR,
+    SECOND_FLOOR
+}
+
+class Configuration {
+    private final String status;
+    private final SomeEnum floor;
+
+    public Configuration(String status, SomeEnum floor) {
+        this.status = status;
+        this.floor = floor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public SomeEnum getFloor() {
+        return floor;
+    }
+}
+
+class Floor {
+    private final Configuration configuration;
+    private final HashMap<String, Runnable> actions = new HashMap<>();
+
+    public Floor(Configuration configuration) {
+        this.configuration = configuration;
+
+        actions.put("available", this::itsAvailable);
+        actions.put("unavailable", this::itsUnavailable);
+
+        actions.getOrDefault(configuration.getStatus(), this::unknownStatus).run();
+    }
+
+    public void itsAvailable() {
+        System.out.println("Floor " + configuration.getFloor() + " is working, lets get the party rolling 🕺🕺🕺!!");
+    }
+
+    public void itsUnavailable() {
+        System.out.println("Floor " + configuration.getFloor() + " is bad ): maybe try next floor.");
+    }
+
+    private void unknownStatus() {
+        System.out.println("Unknown status for floor " + configuration.getFloor());
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.err.println("TODO!");
+        Floor floor1 = new Floor(new Configuration("available", SomeEnum.FIRST_FLOOR));
+        Floor floor2 = new Floor(new Configuration("unavailable", SomeEnum.SECOND_FLOOR));
     }
 }
