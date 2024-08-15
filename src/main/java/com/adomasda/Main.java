8c6b22c12d04ed2f23e9bbf72ceefac26b297873
Adomas Daškevičiaus <adomasda@gmail.com>
2024-08-15 13:57:42 +0300

    moved available, unavailable, unknown method states from Floor to FloorHandler, added Elevator

--- src/main/java/com/adomasda/Main.java
+++ src/main/java/com/adomasda/Main.java
@@ -1,6 +1,10 @@
 package com.adomasda;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 enum SomeEnum {
     FIRST_FLOOR,
@@ -10,10 +14,12 @@ enum SomeEnum {
 class Configuration {
     private final String status;
     private final SomeEnum floor;
+    private final Integer number;
 
-    public Configuration(String status, SomeEnum floor) {
+    public Configuration(Integer number ,String status, SomeEnum floor) {
         this.status = status;
         this.floor = floor;
+        this.number = number;
     }
 
     public String getStatus() {
@@ -23,37 +29,67 @@ class Configuration {
     public SomeEnum getFloor() {
         return floor;
     }
+
+    public Integer getNumber() {
+        return number;
+    }
+}
+
+class FloorHandler {
+    public static void itsUnavailable(Elevator elevator, Floor floor) {
+        System.out.println("Floor " + floor.getConfiguration().getFloor() + " is bad ): maybe try next floor.");
+    }
+
+    public static void itsAvailable(Elevator elevator, Floor floor) {
+        System.out.println("Floor " + floor.getConfiguration().getFloor() + " is bad ): maybe try next floor.");
+    }
+
+    public static void unknownStatus(Elevator _unused, Floor floor) {
+        System.out.println("Unknown status for floor " + floor.getConfiguration().getFloor());
+    }
 }
 
 class Floor {
     private final Configuration configuration;
-    private final HashMap<String, Runnable> actions = new HashMap<>();
+    private final HashMap<String, BiConsumer<Elevator, Floor>> actions = new HashMap<>();
 
     public Floor(Configuration configuration) {
         this.configuration = configuration;
 
-        actions.put("available", this::itsAvailable);
-        actions.put("unavailable", this::itsUnavailable);
-
-        actions.getOrDefault(configuration.getStatus(), this::unknownStatus).run();
+        actions.put("unavailable",  FloorHandler::itsUnavailable);
+        actions.put("available", FloorHandler::itsAvailable);
     }
 
-    public void itsAvailable() {
-        System.out.println("Floor " + configuration.getFloor() + " is working, lets get the party rolling 🕺🕺🕺!!");
+    public Configuration getConfiguration () {
+        return configuration;
     }
 
-    public void itsUnavailable() {
-        System.out.println("Floor " + configuration.getFloor() + " is bad ): maybe try next floor.");
+    public void request(Elevator elevator) {
+        BiConsumer<Elevator, Floor> action = actions.getOrDefault(configuration.getStatus(), FloorHandler::unknownStatus);
+        action.accept(elevator, this);
+    }
+}
+
+class Elevator {
+    private final List<Floor> floors;
+
+    public Elevator(Floor... floors) {
+        this.floors = Arrays.asList(floors);
     }
 
-    private void unknownStatus() {
-        System.out.println("Unknown status for floor " + configuration.getFloor());
+    public void gotoFloor(int floorNumber) {
+        floors.get(floorNumber - 1).request(this);
+        // TODO
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Floor floor1 = new Floor(new Configuration("available", SomeEnum.FIRST_FLOOR));
-        Floor floor2 = new Floor(new Configuration("unavailable", SomeEnum.SECOND_FLOOR));
+        Floor floor1 = new Floor(new Configuration(1, "available", SomeEnum.FIRST_FLOOR));
+        Floor floor2 = new Floor(new Configuration(2, "unavailable", SomeEnum.SECOND_FLOOR));
+
+        Elevator elevator = new Elevator(floor1, floor2);
+
+        elevator.gotoFloor(2);
     }
 }
